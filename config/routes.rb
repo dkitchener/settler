e2d928c83f7295014c73dee26afae69cf9d06c39
David Kitchener <kitchener.david@gmail.com>
2020-12-05 18:28:07 -0500

    dashboard route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: redirect('admin')
+
+  get 'dashboard', to: 'dashboard#index'
 end
