ca5fbf36cf781f2b6e26b2213d9d22a6bcce85f7
David Kitchener <kitchener.david@gmail.com>
2020-12-06 02:22:52 -0500

    Make more dynamic

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,8 @@
 class DashboardController < ApplicationController
   def index
-    @current_series = Series.find_by(current: true)    
+    @current_series = Series.find_by(current: true)
+    @ordered_games = @current_series.games
+    @ordered_players = Player.all
+    @ordered_scores = Score.where(game_id: @current_series.games.pluck(:id))
   end
 end
