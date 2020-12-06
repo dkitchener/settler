57fb6335e865cda54626c6908770c66d801936a5
David Kitchener <kitchener.david@gmail.com>
2020-12-05 22:26:34 -0500

    fix up root routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: redirect('admin')
+  root to: 'dashboard#index'
 
   get 'dashboard', to: 'dashboard#index'
 end
